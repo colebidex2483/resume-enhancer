07cf843fbc82284fcd21bba803a4d94dab545860
Cole Bidemi <colebidex@gmail.com>
2025-05-24 05:19:24 +0100

    update the frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,19 +1,10 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
   <div class="w-full m-auto flex flex-col items-center">
-    <!-- <nav class="w-[85%] mt-2 rounded-lg py-4 bg-gradient-to-r from-blue-500 to-purple-600 shadow-md">
-      <div class="container mx-auto flex px-5 justify-between items-center">
-        <div class="text-white text-lg font-bold">Your Logo</div>
-        <div class="flex space-x-8">
-          <RouterLink to="/" class="text-white hover:text-gray-200 font-semibold transition duration-300">Home</RouterLink>
-          <RouterLink to="/about" class="text-white hover:text-gray-200 font-semibold transition duration-300">About</RouterLink>
-        </div>
-      </div>
-    </nav> -->
-    <div class="container mx-auto mt-8">
+    <div class="container mx-auto">
       <RouterView />
     </div>
   </div>
--- frontend/src/components/FileUpload.vue
+++ frontend/src/components/FileUpload.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="mb-10 md:mb-0">
     <div
       @dragover.prevent="isDragging = true"
       @dragleave="isDragging = false"
@@ -9,7 +9,7 @@
         'border-gray-300': !isDragging && !file,
         'border-green-300 bg-green-50': file && !isDragging
       }"
-      class="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-dashed rounded-md transition-colors duration-200"
+      class="mt-1 flex justify-center items-center px-6 md:h-64 pt-10 pb-6 border-2 border-dashed rounded-md transition-colors duration-200"
     >
       <div class="space-y-1 text-center">
         <svg
@@ -34,7 +34,7 @@
         <div class="flex text-sm text-gray-600">
           <label
             for="file-upload"
-            class="relative cursor-pointer rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
+            class="relative cursor-pointer rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-offset-2 focus-within:ring-indigo-500"
           >
             <span>Upload a file</span>
             <input
